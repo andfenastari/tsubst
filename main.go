49ed2cf95e7f1c72f2c4a93311cf2abe95e392dc
Andre <andre@localhost.localdomain>
2021-09-11 20:19:11 +0000

    Run go fmt

--- main.go
+++ main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-  "os"
-  "strings"
-  "text/template"
-  "fmt"
+	"fmt"
+	"os"
+	"strings"
+	"text/template"
 )
 
 func main() {
-  envs := os.Environ()
-  envMap := make(map[string]string)
-  for _, env := range envs {
-    parts := strings.Split(env, "=")
-    envMap[parts[0]] = parts[1]
-  }
-  
-  if len(os.Args) != 2 {
-    fmt.Fprintf(os.Stderr, "USAGE: %s <file>\n", os.Args[0])
-    os.Exit(1)
-  }
-  
-  tmpl, err := template.ParseFiles(os.Args[1])
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "%v\n", err)
-    os.Exit(1)
-  }
-  
-  err = tmpl.Execute(os.Stdout, envMap)
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "%v\n", err)
-    os.Exit(1)
-  }
+	envs := os.Environ()
+	envMap := make(map[string]string)
+	for _, env := range envs {
+		parts := strings.Split(env, "=")
+		envMap[parts[0]] = parts[1]
+	}
+
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "USAGE: %s <file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	tmpl, err := template.ParseFiles(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	err = tmpl.Execute(os.Stdout, envMap)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
